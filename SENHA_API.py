eff38e8f3940c2027e3ba1a7d366763fe5538dc4
Victor Hugo <48137901+dotavieira@users.noreply.github.com>
2023-10-30 11:16:16 -0300

    Adicionado tutorial

--- SENHA_API.py
+++ SENHA_API.py
@@ -3,6 +3,10 @@
 Created on Wed Aug 23 06:16:27 2023
 
 @author: VP
+
+A fim de adquirir uma senha e rodar o programa, o usuário precisa adentrar no site da OpenAi: https://platform.openai.com/
+Após logado no site, o usuário deverá clicar na imagem de seu perfil e ir em 'ver chaves de api'
+Depois de adquirido uma senha própria, substituir a senha abaixo pela adquirida
 """
 
 API_KEY = "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"
